5f332947306494be8fb04805b76f1b179f9146ff
varun6532 <varunsingh1022@gmail.com>
2026-07-18 00:16:42 +0530

    Bootloader: clear screen + reusable print_string routine via call/ret

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,18 +1,28 @@
-org 0x7C00
+org  0x7C00
 
 start:
-    mov si, message
+   mov ah , 0x00 
+   mov al , 0x03
+   int 0x10
+   mov si , message
+   call print_string
+   jmp done
+
 print_string:
-    lodsb
-    cmp al, 0
-    je done
-    mov ah, 0x0E
-    int 0x10
-    jmp print_string
+   lodsb
+   cmp al , 0 
+   je print_done 
+   mov ah , 0x0E
+   int 0x10
+   jmp print_string
+   
+
+print_done:
+   ret 
 done:
-    jmp done
+   jmp done
 
-message: db "Afnex OS Booted", 0
+message : db "Afnex 0S booted with clear the screen." , 0
 
-times 510-($-$$) db 0
+times 510 - ($-$$) db 0
 dw 0xAA55
