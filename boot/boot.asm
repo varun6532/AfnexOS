aa0839e2ac6a918f6c9df3d650d3b165ed31f796
varun6532 <varunsingh1022@gmail.com>
2026-07-20 20:55:35 +0530

    Revert to real mode - keep it simple for a hobby OS

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,14 +8,7 @@ start:
     mov si, message
     call print_string
 
-    cli                     ; disable interrupts before switching modes
-    lgdt [gdt_descriptor]   ; load our GDT
-
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax            ; set the protected mode bit
-
-    jmp CODE_SEG:init_pm    ; far jump into 32-bit code, flushes CPU pipeline
+    jmp done
 
 print_string:
     lodsb
@@ -27,55 +20,10 @@ print_string:
 print_done:
     ret
 
+done:
+    jmp done
+
 message: db "Afnex OS booted with clear the screen.", 0
-
-; ---- GDT ----
-gdt_start:
-    dq 0x0000000000000000
-
-gdt_code:
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10011010b
-    db 11001111b
-    db 0x00
-
-gdt_data:
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10010010b
-    db 11001111b
-    db 0x00
-
-gdt_end:
-
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
-[bits 32]
-init_pm:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-    mov ebp, 0x90000
-    mov esp, ebp
-
-    ; we are now in 32-bit protected mode
-    ; halt here for 
-   mov byte [0xB8000], 'Z'
-   mov byte [0xB8001], 0x0F
-
-    jmp $
 
 times 510-($-$$) db 0
 dw 0xAA55
