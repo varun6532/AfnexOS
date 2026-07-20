0a96519e527640762dc18667232de108bda9a181
varun6532 <varunsingh1022@gmail.com>
2026-07-20 21:12:01 +0530

    Real-mode keyboard echo loop - continuous key read/print

--- boot/boot.asm
+++ boot/boot.asm
@@ -5,25 +5,31 @@ start:
     mov al, 0x03
     int 0x10
 
-    mov si, message
+    mov si , message
     call print_string
+    call keypress
 
     jmp done
-
 print_string:
     lodsb
-    cmp al, 0
-    je print_done
-    mov ah, 0x0E
+    cmp al , 0 
+    je print_done 
+    mov ah , 0x0E
     int 0x10
     jmp print_string
-print_done:
-    ret
 
+keypress:
+    mov ah , 0x00
+    int 0x16
+    mov ah , 0x0E
+    int 0x10
+    jmp keypress
+print_done:
+    ret 
 done:
     jmp done
+message : db "Hello World" , 0
 
-message: db "Afnex OS booted with clear the screen.", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
